3b5798e3f5ca96875f9d1c3293a676d95c806ee3
Shouvik1Sarkar <shouvik3501@gmail.com>
2026-01-15 00:33:22 +0530

    refresh token set

--- models/user.models.js
+++ models/user.models.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema(
       lowercase: true,
       trim: true,
     },
-    fullname: {
+    fullName: {
       type: String,
       required: true,
       unique: true,
@@ -64,7 +64,29 @@ userSchema.methods.isPasswordCorrect = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-userSchema.methods.generateAccessToken = function () {};
-userSchema.methods.refreshAccessToken = function () {};
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.refreshAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 const User = mongoose.model("User", userSchema);
 export default User;
